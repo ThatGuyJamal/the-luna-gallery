2adadfd43f255c44ec7fb6b0582b519d463c5569
ThatGuyJamal <jamallyons8@gmail.com>
2023-05-08 22:00:32 -0400

    updating small things

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 `Luna The Dog`
+
+You can view the amazing luna [@luna.com](https://luna-land.vercel.app/)
--- app/view/[id]/page.tsx
+++ app/view/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { FC } from "react";
 import Image from "next/image";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import Link from "next/link";
 import { useTheme } from "@/hooks/useTheme";
 
@@ -11,31 +11,35 @@ interface pageProps {}
 const ImageView: FC<pageProps> = ({}) => {
 	const { colors } = useTheme();
 	const searchParams = useSearchParams();
+	const router = useRouter();
 
 	const imageSearch = searchParams.get("search");
 
 	if (!imageSearch) {
 		return (
-			<>
-				<h1>404 - This Luna Cant be found!</h1>
-				<Link href="/"> Go Back Home </Link>
-			</>
+			<div className={`flex flex-col h-screen ${colors.bg}`}>
+				<h1 className="text-3xl font-bold my-auto text-center">
+					404 - This Luna Can't be Found!
+				</h1>
+			</div>
 		);
 	}
 
 	return (
-		<div className={`flex justify-center items-center h-screen ${colors.bg}`}>
-			<div className="shadow-lg rounded-md overflow-hidden">
+		<div className={`flex flex-col h-screen ${colors.bg}`}>
+			<div className="my-auto mx-auto">
 				<Link href={"/"}>
 					<Image
 						src={`/images/${imageSearch}.jpg`}
 						width={500}
 						height={500}
 						alt="Luna Image"
-						className="object-cover"
+						className="object-cover rounded-md shadow-lg"
 					/>
 				</Link>
-				<p className="text-sm">Click on luna to go home!</p>
+				<p className="text-sm text-center mt-2">
+					Click on the Luna to go back home!
+				</p>
 			</div>
 		</div>
 	);
